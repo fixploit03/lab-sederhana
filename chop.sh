c838c2847ac2d9614703f03a0cd5e1e3dd390887
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2026-01-10 04:03:00 +0700

    Create chop.sh

---
+++ chop.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#-----------------------------------------------------------
+#
+#.......: chop.sh
+#.......: Script Bash sederhana untuk channel hopping
+#.......: Rofi (Fixploit03)
+#
+#-----------------------------------------------------------
+
+# Interface ambil dari argumen ke 2
+interface=$2
+
+# Fungsi usage
+function usage(){
+    echo "Cara menggunakan: sudo bash $0 -i [interface]"
+    exit 1
+}
+
+# Cek root
+if [[ $EUID -ne 0 ]]; then
+    echo "[-] Script ini harus dijalankan sebagai root!"
+    exit 1
+fi
+
+# Jika panjang argumen tidak sama dengan 2, manggil fungsi usage
+if [[ $# -ne 2  ]]; then
+    usage
+else
+    # Jika panjang argumen 2, tapi argumen 1 bukan -i, manggil fungsi usage
+    if [[ $1 != "-i" ]]; then
+        usage
+    fi
+fi
+
+# Pesan
+echo "[*] Memulai channel hopping pada interface '${interface}'..."
+
+# Pindah channel dari channel 1-11 menggunakan iw via for loop secara terus menerus
+while true; do
+    for channel in {1..11}; do
+        echo "[*] Pindah ke channel: ${channel}"
+        if iw dev "${interface}" set channel "${channel}"; then
+            echo "[+] Berhasil pindah ke channel: ${channel}"
+        else
+            echo "[-] Gagal pindah ke channel: ${channel}"
+            exit 1
+        fi
+        sleep 1
+    done
+done
+
+# Semoga bermanfaat ^_^
